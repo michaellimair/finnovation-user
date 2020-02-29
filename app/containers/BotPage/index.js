7626e53c54af8c0d99c0d2cfe2a3d0ca351924bf
Michael Lim <michaellim@Michaels-MacBook-Pro.local>
2020-02-29 03:01:52 -0500

    initial bot page config

--- app/containers/BotPage/index.js
+++ app/containers/BotPage/index.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
-import AppHeader from '../../components/Header';
-import CustomDrawer from '../../components/Drawer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
+import robotImage from '../../images/trading_bot.jpg';
 
-const BotPage = (props) => {
-  const [drawerOpened, toggleDrawer] = useState(false);
+import './styles.css';
 
+const BotImage = () => (
+  <div id="robot-icon">
+    <img src={robotImage} alt="Robot" />
+  </div>
+);
+
+const BotHeaderDisplay = () => {
+  const botName = "Michael's Bot";
+  const levelString = "Level 10";
   return (
-    <div>
-      Bot
+    <div id="bot-header">
+      <BotImage />
+      <div id="bot-header-info">
+        <div>{botName}</div>
+        <div>{levelString}</div>
+      </div>
     </div>
   );
 }
+
+const BotDetails = (props) => <div>botdetails</div>
+
+const BotPage = (props) => (
+  <div id="bot-page">
+    <BotHeaderDisplay />
+    <BotDetails />
+  </div>
+)
 
 export default BotPage;
