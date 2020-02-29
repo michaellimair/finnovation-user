3c008405c14f854919de5a9af609f12874a850c4
Sarthak Munshi <sarthakmunshi@gmail.com>
2020-02-29 06:00:31 -0500

    Added News Fetch

--- app/containers/HomePage/components/BotStats/index.js
+++ app/containers/HomePage/components/BotStats/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 const BotStats = (props) => (
   <div id="bot-stats" className="dash-column">
     <Card className="dash-column-card">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/LI2BARWJ1a8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/rgVdgR1y1Dg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Card>
   </div>
 )
